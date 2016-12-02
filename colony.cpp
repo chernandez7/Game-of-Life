47761afdb4b46b780107ef5e11684af21dc2763a
chernandez7 <chernandez7@luc.edu>
2016-12-02 12:03:27 -0600

    cleaned up comments

--- colony.cpp
+++ colony.cpp
@@ -20,7 +20,6 @@ Colony::Colony(int length, int width) {
       this->gen0[i] = new int[this->width];
       this->currentGrid[i] = new int[this->width];
   }
-  std::cout << "Dynamic allocation worked" << std::endl;
   // init values to 0
   for(int k = 0; k < this->length; k++) {
     for(int j = 0; j < this->width; j++) {
@@ -28,31 +27,29 @@ Colony::Colony(int length, int width) {
       currentGrid[k][j] = 1;
     }
   }
-  std::cout << "init worked" << std::endl;
-
-  //_copyGrid(gen0, this->gen0);
-  //std::cout << "copy1 worked" << std::endl;
-
-  //_copyGrid(this->gen0, this->currentGrid); // current grid at init is gen0
-  //std::cout << "copy2 worked" << std::endl;
-
-  std::cout << "Colony created with dimensions " << length << "x" << width  << std::endl;
 }
 
 Colony::~Colony() {
-  /*free up memory
+  // free up memory
   for(int i = 0; i < this->length; ++i) {
     delete [] this->gen0[i];
     delete [] this->currentGrid[i];
   }
   delete [] this->gen0;
   delete [] this->currentGrid;
-  */
+
 }
 
 void Colony::life(int** grid) {}
 
 void Colony::printGrid() {
+  /* top border
+  for (int k = 0; k < this->width; k++) {
+    std::cout << "-";
+  }
+  std::cout << std::endl;*/
+
+
   for(int j = 0; j < this->length; j++) { // ignore first and last lines
  		for(int i = 0; i < this->width; i++) {
 			if(this->currentGrid[j][i] == 1) {
@@ -63,6 +60,12 @@ void Colony::printGrid() {
     }
 		std::cout << std::endl;
  	}
+
+  /* Bottom border
+  for (int k = 0; k < this->width; k++) {
+    std::cout << "-";
+  }
+  std::cout << std::endl;*/
 }
 
 int Colony::_getLength(int** grid) {
@@ -78,7 +81,6 @@ int Colony::_getWidth(int** grid) {
 void Colony::_copyGrid(int** original, int** temp) {
   if (_getLength(original) == _getLength(temp) &&
       _getWidth(original) == _getWidth(temp)) { // check that both have same dimensions
-        std::cout << "both grids share dimensions" << std::endl;
     for(int j = 0; j < _getLength(original); j++) { // iterated through every j and i
   		for(int i = 0; i < _getWidth(original); i++)
   			temp[j][i] = original[j][i];
